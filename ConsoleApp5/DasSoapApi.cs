340adb1d7b3a90efe761e606ddda1fe7fa3a5e6a
Antony Capper <acapper56@gmail.com>
2019-07-04 23:04:59 +0100

    Initial commit

---
+++ ConsoleApp5/DasSoapApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class DasSoapApi
+    {
+		public string Method1(string message1) {
+			return "I'm function Method1: " + message1;
+		}
+
+		public string Method2(string message2) {
+			return "I'm function Method2: " + message2;
+		}
+
+		public string Method3(string message3) {
+			return "I'm function Method3: " + message3;
+		}
+
+	}
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DasSoapApi das = new DasSoapApi();
+            Type thisType = das.GetType();
+
+            using (StreamReader r = new StreamReader("api.json"))
+            {
+                string jsonText = r.ReadToEnd();
+                JObject json = JObject.Parse(jsonText);
+                foreach (var i in json.SelectToken("apiEndPoints"))
+                {
+                    MethodInfo theMethod = thisType.GetMethod(i.SelectToken("name").ToString());
+                    var res = theMethod.Invoke(das, new object[] { "Message" });
+
+                    Console.WriteLine(i.SelectToken("name").ToString());
+                    Console.WriteLine(res);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
